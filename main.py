24914ca928b36bd0950fee2274c7e1f616f7a4b4
Laxman Sharma <laxman.sharma1811@gmail.com>
2026-04-06 00:40:48 +0545

    solving error for deployment

--- main.py
+++ main.py
@@ -131,4 +131,6 @@ async def handle_form(request: Request, query: str = Form(...)):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
